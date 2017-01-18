4c0789425fdae94e35883bfa7e82a01c3823ef35
Fuuccker <faca981@gmail.com>
2017-01-18 16:54:07 -0300

    Refactoring 'bank.py'

--- stock-alarm/bank.py
+++ stock-alarm/bank.py
@@ -1,72 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Bank():
+class Bank(object):
+    allowed_values = [
+        'name',
+        'Fee',
+        'NIM'
+    ]
+    
     def __init__(self, name):
         self.name = name.upper()
         self.values = {}
 
-    def name(self):
-        return self.name
-
-    def add_fee(self, fee):
-        self.values['Fee'] = float(fee)
-
-    def get_fee(self):
-        return self.values['Fee']
-
-    def add_tna(self, tna):
-        self.values['TNA'] = float(tna)
-
-    def get_tna(self):
-        return self.values['TNA']
-
     def __str__(self):
-        s = ""
-        s += "\n  Bank: %s" % self.name
-        for key, value in self.values.iteritems():
-            s += "\n    %s: %.4f%%" % (key, value)
-        return s
+        raise NotImplementedError
 
-class Banks():
+    def get(self, attr):
+        if attr in self.allowed_values:
+            if attr == "Fee":
+                return self.values['Fee']
+            elif attr == "NIM":
+                return self.values['NIM']
+            else:
+                return getattr(self, attr)
+        else:
+            return None
+
+    def add(self, attr, value):
+        if attr in self.allowed_values:
+            if attr == "Fee":
+                self.values['Fee'] = value
+            elif attr == "NIM":
+                self.values['NIM'] = value
+
+
+class Banks(object):
+    allowed_values = [
+        'name',
+        'Fee',
+        'NIM'
+    ]
+
     def __init__(self):
         self.banks = {}
 
     def __str__(self):
-        s = ""
-        s += "-- BANKS --\n"
-        for bank in self.banks:
-            s += self.banks[bank].__str__()
-        return s
+        raise NotImplementedError
 
     def get_bank(self, bank):
         if bank in self.banks:
-            return self.banks[bank]
+            return self.banks[bank.upper()]
         else:
             return None
 
-    def add_fees(self, dict_with_values):
-        for bank, value in dict_with_values:
+    def get(self, bank, attr):
+        if bank not in self.banks:
+            return None
+        elif attr in self.allowed_values:
+            if attr == "NIM":
+                return self.get_bank(bank).get('NIM')
+            elif attr == "Fee":
+                # print self.get_bank(bank).get('Fee')
+                return self.get_bank(bank).get('Fee')
+            else:
+                return None
+
+    def add_fees(self, values):
+        for bank, value in values:
             bank = bank.upper()
             if bank not in self.banks:
                 self.banks[bank] = Bank(bank)
-            self.banks[bank].add_fee(float(value))
+            self.get_bank(bank).add('Fee', float(value))
 
-    def add_tna(self, dict_with_values):
-        for bank, value in dict_with_values:
+    def add_tna(self, values):
+        for bank, value in values:
             bank = bank.upper()
             if bank not in self.banks:
                 self.banks[bank] = Bank(bank)
-            self.banks[bank].add_tna(float(value))
-
-    def get_tna(self, bank):
-        if bank not in self.banks:
-            return None
-        else:
-            return self.banks[bank].get_tna()
-
-    def get_fee(self, bank):
-        if bank not in self.banks:
-            return None
-        else:
-            return self.banks[bank].get_fee()
+            self.get_bank(bank).add('NIM', float(value))
